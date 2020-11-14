ca598ba9388d65c086f6057fec84ded76a8d7046
Anthony Chow <anthonychow@Anthonys-Mac-mini.local>
2020-11-14 14:43:44 -0500

    add comments to make the i/o interface clearer

--- KuzminPotential/codes/macer_poisson.hpp
+++ KuzminPotential/codes/macer_poisson.hpp
@@ -17,9 +17,12 @@
 
     //*********************************************************************************
     // Get parameters (total mass and scale length) for Kuzmin disk potential
-    // Input arguments: Two dimentional array stellarMass[row][col] containing
+    // Input: 
+    //    Two dimentional array stellarMass[row][col] containing
     //    stellar masses (without ghost cells) between upper and lower   
     //    cells in theta direction (row) and all cells in radial direction (col)
+    // Output:
+    //    Kuzmin_in structure containing total mass (M) and length scale (a)
     //*********************************************************************************
     template <int rows, int cols>
     Kuzmin_in getKuzminParams(MeshBlock *pmb, Real (&stellarMass)[rows][cols])
@@ -50,7 +53,7 @@
       s.a = pmb->pcoord->x1v(ihalfRadius + pmb->is) / sqrt(3); 
       return s;
     }
-
+   
 
 
 
@@ -60,6 +63,10 @@
     //        i = index to radial component of the cells where gradient phi is computed
     //        j = index to theta component of the cells where gradient phi is computed
     //        Kuzim_in = structure contains the Kuzim disk parameters
+    // Output:
+    //        Kuzmin_out structure contains radial and azimuthal components of force per
+    //        unit mass (negative of analytical gradient of Kuzmin disk potential) at 
+    //        location indicated by radial and azimutal index i & j
     //********************************************************************************* 
     Kuzmin_out getForce(MeshBlock *pmb, int i, int j, Kuzmin_in s_in, Real G)
     {
